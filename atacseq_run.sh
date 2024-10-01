84ede26d2810b0e22c775cf40557872c82dfb8ee
chaochaowong <chao-jen.wong@seattlechildrens.org>
2024-10-01 10:19:18 -0700

    edit atacseq_nextflow.config such that it can run the default testing

--- atacseq_run.sh
+++ atacseq_run.sh
@@ -3,18 +3,15 @@
 set -eu
 DATE=$(date +%F)
 NFX_CONFIG=./atacseq_nextflow.config
-#Options: "atac_seq"
-NFX_ENTRY="atac_seq"
 #The output prefix on filenames for reports/logs
 REPORT=${1:-"pipeline_report"}
 
 # Nextflow run to execute the workflow 
 # TO DO: --singularity_module $SINGULARITY #the in nextflow.config could access this as params.SINGULARITY
 PREFIX="${REPORT}_${DATE}"
-nextflow run nf-cor/atacset \
+nextflow run nf-core/atacseq \
+    -r 2.1.2 \
     -c ${NFX_CONFIG} \
-    -log reports/${PREFIX}_nextflow.log \
-    -entry ${NFX_ENTRY} \
     -profile sasquatch \
     -with-report reports/${PREFIX}_report.html \
     -with-dag dag/${PREFIX}_dag.html \
